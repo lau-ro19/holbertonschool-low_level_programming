26321187bdcf1ed682abb47b3af26f5450703b56
lau-ro19 <lroseantoinette@gmail.com>
2026-02-24 15:59:28 +0000

    Write a function that checks for lowercase character.

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * int _islower(int c);- Prints the alphabet in lowercase
+ * Description: This function uses a loop to print all letters
+ * from 'a' to 'z'x10 followed by a new line.
+ */
+
+ int _islower(int c);
+ 
