0f2a7ae976745dc1313862565d4d574b840e6ffe
lau-ro19 <lroseantoinette@gmail.com>
2026-02-25 14:54:50 +0000

    Write a function that prints the numbers, from 0 to 9, followed by a new line.

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -3,7 +3,8 @@
 /**
  * print_most_numbers: Write a function that prints some numbers.
  *
- * Description: prints the numbers, from 0 to 9 and 4 followed by a new line but do not print 2 and 4. 
+ * Description: prints the numbers,from 0 to 9 and 4 
+ * followed by a new line but do not print 2 and 4.
  * You can only use _putchar twice.
  */
 
