4629bf31fc4a28559e66c8638e2a5723efb428f6
lau-ro19 <lroseantoinette@gmail.com>
2026-03-04 09:35:53 +0000

    Write a function that copies a string.
    Prototype: char *_strncpy(char *dest, char *src, int n);
    Your function should work exactly like strncpy
    FYI: The standard library provides a similar function: strncpy. Run man strncpy to learn more.

--- pointers_arrays_strings2/2-strncpy.c
+++ pointers_arrays_strings2/2-strncpy.c
@@ -12,15 +12,12 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int j = 0;
 
-	/* 1. Copie src vers dest jusqu'à n ou jusqu'à la fin de src */
 	while (j < n && src[j] != '\0')
 	{
 		dest[j] = src[j];
 		j++;
 	}
 
-	/* 2. Si on a atteint la fin de src mais pas encore n, */
-	/* on remplit le reste de dest avec des '\0' */
 	while (j < n)
 	{
 		dest[j] = '\0';
