bf025db24b3a6498abc097b0ad431da43010447d
lau-ro19 <lroseantoinette@gmail.com>
2026-03-05 08:07:27 +0000

    putchar

---
+++ pointers_arrays_strings/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+
+/**
+ * _putchar - écrit le caractère c sur stdout
+ * @c: Le caractère à imprimer
+ * Return: 1 en cas de succès, -1 en cas d'erreur
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
