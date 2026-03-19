1d16274990d342d0292d42e9942aa77c5598ea3a
lau-ro19 <lroseantoinette@gmail.com>
2026-03-19 16:52:44 +0100

    Use strdup for name and owner allocation in new_dog function

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -17,21 +17,20 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
-	dog->name = malloc(strlen(name) + 1);
+	dog->name = strdup(name);
 	if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
-	dog->owner = malloc(strlen(owner) + 1);
+	dog->owner = strdup(owner);
 	if (dog->owner == NULL)
 	{
 		free(dog->name);
 		free(dog);
 		return (NULL);
 	}
-	strcpy(dog->name, name);
-	strcpy(dog->owner, owner);
+
 	dog->age = age;
 	return (dog);
 }
