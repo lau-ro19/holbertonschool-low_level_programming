181c4783d05eac0dfc239370ae6d5e501419544b
lau-ro19 <lroseantoinette@gmail.com>
2026-03-19 16:47:57 +0100

    Refactor struct dog documentation and typedef for clarity

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -5,23 +5,17 @@
 #include <string.h>
 
 /**
- * struct dog - A new type describing a dog
- * @name: First member (char *)
- * @age: Second member (float)
- * @owner: Third member (char *)
- *
- * Description: Header file that defines a new type struct dog
- * and an alias dog_t.
+ * struct dog - a dog structure
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
-/* Def du nouvel alias dog_t pour struct dog */
-typedef struct dog dog_t;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
