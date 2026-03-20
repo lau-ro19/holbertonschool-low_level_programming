d8fc200e15e8067d40d0e5e9dc66c175ee98f711
lau-ro19 <lroseantoinette@gmail.com>
2026-03-20 19:03:33 +0100

    Fix missing newlines at the end of files in create_array, str_concat, and alloc_grid functions

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -23,7 +23,7 @@ int **alloc_grid(int width, int height)
     for (i = 0; i < height; i++)
     {
         grid[i] = malloc(sizeof(int) * width);
-        
+
         if (grid[i] == NULL)
         {
             for (; i >= 0; i--)
