d863da1c3589aa47623b55c5e7191ecf25bfc690
lau-ro19 <lroseantoinette@gmail.com>
2026-03-20 19:17:23 +0100

    Remove unnecessary comments and improve code clarity in create_array function

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -13,24 +13,19 @@ char *create_array(unsigned int size, char c)
 	char *str;
 	unsigned int i;
 
-	/* 1. Sécurité : si la taille est 0, on ne fait rien */
+
 	if (size == 0)
 		return (NULL);
 
-	/* 2. Allocation de la mémoire */
-	/* On multiplie par sizeof(char) pour être propre, même si c'est 1 octet */
 	str = malloc(sizeof(char) * size);
 
-	/* 3. Vérification de l'allocation */
 	if (str == NULL)
 		return (NULL);
 
-	/* 4. Initialisation du tableau avec le caractère c */
 	for (i = 0; i < size; i++)
 	{
 		str[i] = c;
 	}
 
-	/* 5. On retourne le pointeur vers le début de la zone mémoire */
 	return (str);
 }
