3fdc5314a13cced0234052113a11cb0b7d83ba26
lau-ro19 <lroseantoinette@gmail.com>
2026-03-20 19:14:06 +0100

    Fix formatting and add newlines in main.h header file

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -1,19 +1,25 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 /**
  * File: main.h
  * Desc: Header file containing prototypes for all functions
  * used in the 0x0a-malloc_free directory.
  */
 
+/* 0. create_array */
 char *create_array(unsigned int size, char c);
 
+/* 1. _strdup */
 char *_strdup(char *str);
 
+/* 2. str_concat */
 char *str_concat(char *s1, char *s2);
 
+/* 3. alloc_grid */
 int **alloc_grid(int width, int height);
 
+/* 4. free_grid */
 void free_grid(int **grid, int height);
 
-#endif
+#endif /* MAIN_H */
