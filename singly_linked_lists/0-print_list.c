7546258b0a23d68505c7d88e148019eadd3a1851
lau-ro19 <lroseantoinette@gmail.com>
2026-03-27 12:57:05 +0100

    simplification of the boucle

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -4,23 +4,23 @@
 
 /**
  * print_list - prints all the elements of a list_t list
- * @h: pointer to the list_t list
+ * @h: pointer to the head of the list
  *
  * Return: the number of nodes
  */
 size_t print_list(const list_t *h)
 {
-	size_t s = 0;
+	size_t n = 0;
 
 	while (h)
 	{
-		if (!h->str)
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
-		s++;
+		n++;
 	}
 
-	return (s);
+	return (n);
 }
