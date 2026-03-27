e729940a8f8461d0d9fec9f1e932a6aba490173c
lau-ro19 <lroseantoinette@gmail.com>
2026-03-27 12:21:14 +0100

    add more description

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -9,18 +9,26 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	size_t nodes = 0;
 
+	/* Loop through the list until the end (NULL) */
 	while (h != NULL)
 	{
+		/* Check if string is NULL to print (nil) */
 		if (h->str == NULL)
+		{
 			printf("[0] (nil)\n");
+		}
 		else
+		{
+			/* Print length and string content */
 			printf("[%u] %s\n", h->len, h->str);
+		}
 
-		count++;
+		/* Move to the next node and increment counter */
+		nodes++;
 		h = h->next;
 	}
 
-	return (count);
+	return (nodes);
 }
