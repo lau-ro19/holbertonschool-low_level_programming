0681dec3ba51ed14de45bd901b4006f1c9efdb70
lau-ro19 <lroseantoinette@gmail.com>
2026-04-02 14:41:22 +0200

    remove some description

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -24,7 +24,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (ht->array == NULL)
 	{
-		free(ht); /* Clean up previously allocated table if array fails */
+		free(ht);
 		return (NULL);
 	}
 
