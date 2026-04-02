e8dcb7f1b025da7bbeb6e87e2221dfe2b8eff239
lau-ro19 <lroseantoinette@gmail.com>
2026-04-02 14:26:11 +0200

    Write a function that creates a hash table. Prototype: hash_table_t *hash_table_create(unsigned long int size); where size is the size of the array Returns a pointer to the newly created hash table If something went wrong, your function should return NULL

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: the size of the array
+ *
+ * Return: pointer to the newly created hash table, or NULL if it fails
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+	unsigned long int i;
+
+	if (size == 0)
+		return (NULL);
+
+	/* 1. Allocate the hash table structure */
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+
+	/* 2. Allocate the array of pointers (the buckets) */
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+	{
+		free(ht); /* Clean up previously allocated table if array fails */
+		return (NULL);
+	}
+
+	/* 3. Initialize all pointers in the array to NULL */
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
+
+	ht->size = size;
+
+	return (ht);
+}
