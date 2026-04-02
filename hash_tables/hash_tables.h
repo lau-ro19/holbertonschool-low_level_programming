651da7ffa427a10b35125048fd815c7086bcb7e4
lau-ro19 <lroseantoinette@gmail.com>
2026-04-02 14:52:53 +0200

    remove some space

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -9,10 +9,12 @@
  */
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
-} hash_node_t;
+    char *key;
+    char *value;
+    struct hash_node_s *next;
+} 
+
+hash_node_t;
 
 /**
  * struct hash_table_s - Hash table data structure
@@ -21,9 +23,11 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
-} hash_table_t;
+    unsigned long int size;
+    hash_node_t **array;
+} 
+
+hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
 
