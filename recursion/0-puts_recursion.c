d8ed7df5e17684ab1ab5339fcbd6a2a9949d87b7
lau-ro19 <lroseantoinette@gmail.com>
2026-04-26 16:24:29 +0200

    Fix betty-doc warning by adding function description

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * _puts_recursion - Prints a string, followed by a new line.
+ * @s: The string to be printed.
+ *
+ * Return: void.
+ */
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
