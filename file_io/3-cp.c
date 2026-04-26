454ee57d6485dbab1282248a9ca8f4274d00cf06
lau-ro19 <lroseantoinette@gmail.com>
2026-04-26 17:16:25 +0200

    Add program that copies the content of a file to another

---
+++ file_io/3-cp.c
@@ -0,0 +1,68 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * main - Copies the content of a file to another file.
+ * @ac: Argument count.
+ * @av: Argument vector.
+ *
+ * Return: 0 on success.
+ */
+int main(int ac, char **av)
+{
+	int fd_from, fd_to, n_read, n_wrote;
+	char buffer[1024];
+
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	fd_from = open(av[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+
+	fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+
+	while ((n_read = read(fd_from, buffer, 1024)) > 0)
+	{
+		n_wrote = write(fd_to, buffer, n_read);
+		if (n_wrote == -1 || n_wrote != n_read)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			exit(99);
+		}
+	}
+
+	if (n_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+
+	if (close(fd_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
+	}
+
+	if (close(fd_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
+	}
+
+	return (0);
+}
