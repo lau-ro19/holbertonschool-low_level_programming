a8bf1512324ba57289227887be353f9dee79151d
lau-ro19 <lroseantoinette@gmail.com>
2026-04-26 17:18:46 +0200

    FIX betty style

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -9,52 +9,36 @@
  * @letters: The number of letters it should read and print.
  *
  * Return: The actual number of letters it could read and print.
- * 0 if the file cannot be opened or read, if filename is NULL,
- * or if write fails.
+ * 0 if failure.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t n_read, n_wrote;
+	ssize_t n_r, n_w;
 	char *buffer;
 
 	if (filename == NULL)
 		return (0);
-
-	/* 1. Allouer de l'espace pour les lettres */
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
-
-	/* 2. Ouvrir le fichier */
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
 		free(buffer);
 		return (0);
 	}
-
-	/* 3. Lire le fichier */
-	n_read = read(fd, buffer, letters);
-	if (n_read == -1)
+	n_r = read(fd, buffer, letters);
+	if (n_r == -1)
 	{
 		free(buffer);
 		close(fd);
 		return (0);
 	}
-
-	/* 4. Écrire sur la sortie standard (STDOUT_FILENO) */
-	n_wrote = write(STDOUT_FILENO, buffer, n_read);
-	if (n_wrote == -1 || n_wrote != n_read)
-	{
-		free(buffer);
-		close(fd);
-		return (0);
-	}
-
-	/* 5. Nettoyage */
+	n_w = write(STDOUT_FILENO, buffer, n_r);
 	free(buffer);
 	close(fd);
-
-	return (n_wrote);
+	if (n_w == -1 || n_w != n_r)
+		return (0);
+	return (n_w);
 }
--- file_io/main.h
+++ file_io/main.h
@@ -4,20 +4,19 @@
 #include <stddef.h>
 #include <sys/types.h>
 
-/* --- Prototypes des tâches de File I/O --- */
+/* --- Prototypes --- */
 
-/* Tâche 0: read_textfile */
+/* Tâche 0 */
 ssize_t read_textfile(const char *filename, size_t letters);
 
-/* Tâche 1: create_file */
+/* Tâche 1 */
 int create_file(const char *filename, char *text_content);
 
-/* Tâche 2: append_text_to_file */
+/* Tâche 2 */
 int append_text_to_file(const char *filename, char *text_content);
 
-/* Tâche 3: cp (Note: cp est un exécutable, la fonction main est dans 3-cp.c) */
+/* Tâche 3: Program cp */
 
-/* --- Fonctions Helper (si nécessaire) --- */
 int _putchar(char c);
 
 #endif /* MAIN_H */
