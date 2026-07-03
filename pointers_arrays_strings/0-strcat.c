fb30e6575ed03ad088691b5e778121769205b8a2
lau-ro19 <lroseantoinette@gmail.com>
2026-07-03 09:58:55 +0200

    delete: the extra pointers_arrays_strings file

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -2,29 +2,31 @@
 
 /**
  * _strcat - concatenates two strings
- * @dest: the destination string to be appended to
- * @src: the source string to append
+ * @dest: the destination string
+ * @src: the source string
  *
- * Return: a pointer to the resulting string dest
+ * Return: pointer to the resulting string dest
  */
 char *_strcat(char *dest, char *src)
 {
-	int i = 0;
-	int j = 0;
+	int i;
+	int j;
 
-	while (dest[i] != '\0')
+	i = 0;
+	while (dest[i] != 0)
 	{
 		i++;
 	}
 
-	while (src[j] != '\0')
+	j = 0;
+	while (src[j] != 0)
 	{
 		dest[i] = src[j];
 		i++;
 		j++;
 	}
 
-	dest[i] = '\0';
+	dest[i] = 0;
 
 	return (dest);
 }
